acaf276ac268269fa866c3bd2ea8a5cf5951df0a
Umaima Kaderi <umaima.kaderi@gmail.com>
2019-01-02 16:00:53 +0530

    Procfile n requirements.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,6 @@ EMAIL_USE_TLS = True
 #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_HOST_USER = 'umaima.kaderi@gmail.com'
 EMAIL_HOST_PASSWORD = 'utdnetzg'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
